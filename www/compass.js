a81a1431d0fbcc2cfb4f600d707b34e7c89eee31
hermwong <herm.wong@gmail.com>
2013-05-13 13:26:23 -0700

    update require path to CompassHeading & CompassError classes

--- www/compass.js
+++ www/compass.js
@@ -22,8 +22,8 @@
 var argscheck = require('cordova/argscheck'),
     exec = require('cordova/exec'),
     utils = require('cordova/utils'),
-    CompassHeading = require('cordova/plugin/CompassHeading'),
-    CompassError = require('cordova/plugin/CompassError'),
+    CompassHeading = require('www/CompassHeading'),
+    CompassError = require('www/CompassError'),
     timers = {},
     compass = {
         /**
